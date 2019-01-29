cfa098f76d1027495c107a337616b7b432c4b45c
haiaimi <wanghai1997@outlook.com>
2019-01-29 22:16:39 +0800

    添加动画实例类，为了适配AI

---
+++ ActionGame/Private/Player/ActionAnimInstance_Aurora.cpp
@@ -0,0 +1,105 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ActionAnimInstance_Aurora.h"
+#include "ActionGameCharacter_Aurora.h"
+#include <Kismet/GameplayStatics.h>
+#include "GameFramework/CharacterMovementComponent.h"
+#include <Particles/ParticleSystem.h>
+
+UActionAnimInstance_Aurora::UActionAnimInstance_Aurora()
+{
+	
+}
+
+void UActionAnimInstance_Aurora::AnimNotify_CanStartAttack(UAnimNotify* Notify)
+{
+	APawn* Owner = TryGetPawnOwner();
+	if(AActionGameCharacter_Aurora* CurOwner=Cast<AActionGameCharacter_Aurora>(Owner))
+	{
+		CurOwner->PlayerCanAttack();
+	}
+}
+
+void UActionAnimInstance_Aurora::AnimNotify_SaveAttack(UAnimNotify* Notify)
+{
+	APawn* Owner = TryGetPawnOwner();
+	if(AActionGameCharacter_Aurora* CurOwner=Cast<AActionGameCharacter_Aurora>(Owner))
+	{
+		CurOwner->ComboAttackSave();
+	}
+}
+
+void UActionAnimInstance_Aurora::AnimNotify_ResetCombo(UAnimNotify* Notify)
+{
+	APawn* Owner = TryGetPawnOwner();
+	if(AActionGameCharacter_Aurora* CurOwner=Cast<AActionGameCharacter_Aurora>(Owner))
+	{
+		CurOwner->ResetCombo();
+	}
+}
+
+void UActionAnimInstance_Aurora::AnimNotify_StartMove(UAnimNotify* Notify)
+{
+	APawn* Owner = TryGetPawnOwner();
+	if(AActionGameCharacter_Aurora* CurOwner=Cast<AActionGameCharacter_Aurora>(Owner))
+	{
+		CurOwner->GetCharacterMovement()->MaxWalkSpeed = 500.f;
+		CurOwner->bInAbility = false;
+	}
+}
+
+void UActionAnimInstance_Aurora::AnimNotify_StopFly(UAnimNotify* Notify)
+{
+	APawn* Owner = TryGetPawnOwner();
+	if(AActionGameCharacter_Aurora* CurOwner=Cast<AActionGameCharacter_Aurora>(Owner))
+	{
+		CurOwner->bTurboJumpAccelerate = false;
+	}
+}
+
+void UActionAnimInstance_Aurora::AnimNotify_EndQAbility(UAnimNotify* Notify)
+{
+	APawn* Owner = TryGetPawnOwner();
+	if(AActionGameCharacter_Aurora* CurOwner=Cast<AActionGameCharacter_Aurora>(Owner))
+	{
+		CurOwner->bTurboJumpAccelerate = false;
+		CurOwner->bInAbility = false;
+	}
+}
+
+void UActionAnimInstance_Aurora::AnimNotify_RAbilityWarmUp(UAnimNotify* Notify)
+{
+	APawn* Owner = TryGetPawnOwner();
+	if (AActionGameCharacter_Aurora* CurOwner = Cast<AActionGameCharacter_Aurora>(Owner))
+	{
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), WarmUpParticle, CurOwner->GetActorTransform());
+	}
+}
+
+void UActionAnimInstance_Aurora::AnimNotify_RAbilityBlast(UAnimNotify* Notify)
+{
+	APawn* Owner = TryGetPawnOwner();
+	if (AActionGameCharacter_Aurora* CurOwner = Cast<AActionGameCharacter_Aurora>(Owner))
+	{
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), BlastParticle, CurOwner->GetActorTransform());
+	}
+}
+
+void UActionAnimInstance_Aurora::AnimNotify_FreezeEnemy(UAnimNotify* Notify)
+{
+	APawn* Owner = TryGetPawnOwner();
+	if (AActionGameCharacter_Aurora* CurOwner = Cast<AActionGameCharacter_Aurora>(Owner))
+	{
+		CurOwner->FreezeEnemy();
+	}
+}
+
+void UActionAnimInstance_Aurora::AnimNotify_EndRAbility(UAnimNotify* Notify)
+{
+	APawn* Owner = TryGetPawnOwner();
+	if (AActionGameCharacter_Aurora* CurOwner = Cast<AActionGameCharacter_Aurora>(Owner))
+	{
+		CurOwner->GetCharacterMovement()->JumpZVelocity = 600.f;    //恢复人物跳跃高度
+		CurOwner->bInAbility = false;
+	}
+}
---
+++ ActionGame/Public/Player/ActionAnimInstance_Aurora.h
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ActionAnimInstance_Aurora.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONGAME_API UActionAnimInstance_Aurora : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	UActionAnimInstance_Aurora();
+
+	UFUNCTION()
+	void AnimNotify_CanStartAttack(UAnimNotify* Notify);
+
+	UFUNCTION()
+	void AnimNotify_SaveAttack(UAnimNotify* Notify);
+
+	UFUNCTION()
+	void AnimNotify_ResetCombo(UAnimNotify* Notify);
+
+	UFUNCTION()
+	void AnimNotify_StartMove(UAnimNotify* Notify);
+
+	UFUNCTION()
+	void AnimNotify_StopFly(UAnimNotify* Notify);
+
+	UFUNCTION()
+	void AnimNotify_EndQAbility(UAnimNotify* Notify);
+
+	UFUNCTION()
+	void AnimNotify_RAbilityWarmUp(UAnimNotify* Notify);
+
+	UFUNCTION()
+	void AnimNotify_RAbilityBlast(UAnimNotify* Notify);
+
+	UFUNCTION()
+	void AnimNotify_FreezeEnemy(UAnimNotify* Notify);
+
+	UFUNCTION()
+	void AnimNotify_EndRAbility(UAnimNotify* Notify);
+
+protected:
+	UPROPERTY(EditDefaultsOnly)
+	class UParticleSystem* WarmUpParticle;
+
+	UPROPERTY(EditDefaultsOnly)
+	class UParticleSystem* BlastParticle;
+};
