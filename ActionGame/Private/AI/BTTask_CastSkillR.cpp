b0bdbe4f84c29f002462000365a678efbfb4fcf3
haiaimi <wanghai1997@outlook.com>
2019-01-31 21:36:19 +0800

    添加R技能Task

---
+++ ActionGame/Private/AI/BTTask_CastSkillR.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AI/BTTask_CastSkillR.h"
+#include "ActionAIController.h"
+#include "ActionGameCharacter.h"
+#include <BehaviorTree/BehaviorTreeTypes.h>
+#include "BehaviorTree/Blackboard/BlackboardKeyAllTypes.h"
+#include <GameFramework/CharacterMovementComponent.h>
+
+EBTNodeResult::Type UBTTask_CastSkillR::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AActionAIController* MyController = Cast<AActionAIController>(OwnerComp.GetAIOwner());
+	if (MyController == nullptr)
+		return EBTNodeResult::Failed;
+
+	AActionGameCharacter* MyBot = Cast<AActionGameCharacter>(MyController->GetPawn());
+	AActionGameCharacter* Enemy = MyController->GetEnemy();
+
+	if(Enemy && MyBot)
+	{
+		if ((Enemy->GetActorLocation() - MyBot->GetActorLocation()).Size2D() < 400.f)
+		{
+			if (MyBot->IsAbilityinCooling(2))
+				return EBTNodeResult::Failed;
+
+			MyBot->Ability_R();
+			return EBTNodeResult::Succeeded;
+		}
+	}
+	
+	return EBTNodeResult::Failed;
+}
---
+++ ActionGame/Public/AI/BTTask_CastSkillR.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTTask_CastSkillR.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONGAME_API UBTTask_CastSkillR : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+	
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+};
